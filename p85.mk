d04ce8e0eb161344a07cf1750d41f0ce577c9231
deathscythe <chocolate.menghang@gmail.com>
2012-11-05 22:47:26 +0800

    init

---
+++ p85.mk
@@ -0,0 +1,146 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES := \
+	device/allwinner/p85/kernel:kernel \
+	device/allwinner/p85/init.rc:root/init.rc \
+	device/allwinner/p85/init.sun4i.rc:root/init.sun4i.rc \
+	device/allwinner/p85/init.sun4i.usb.rc:root/init.sun4i.usb.rc \
+	device/allwinner/p85/ueventd.sun4i.rc:root/ueventd.sun4i.rc
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.root_access=3 \
+	persist.service.adb.enable=1 \
+	ro.opengles.version = 131072 \
+	debug.egl.hw=1 \
+	ro.display.switch=1 \
+	ro.sf.lcd_density=160 \
+	hwui.render_dirty_regions=false \
+	wifi.interface = wlan0 \
+	wifi.supplicant_scan_interval = 120 \
+	persist.sys.strictmode.visual=0 \
+	persist.sys.strictmode.disable=1 \
+	persist.sys.usb.config=mass_storage,adb \
+	dalvik.vm.verify-bytecode=false \
+	dalvik.vm.dexopt-flags=v=n,o=v \
+	dalvik.vm.execution-mode=int:jit \
+	persist.sys.timezone=Asia/Shanghai \
+	persist.sys.language=zh \
+	persist.sys.country=CN \
+	ro.com.google.locationfeatures=1 \
+	dalvik.vm.lockprof.threshold=500 \
+	ro.kernel.android.checkjni=0 \
+	dalvik.vm.checkjni=false \
+	dalvik.vm.dexopt-data-only=1 \
+	ro.vold.umsdirtyratio=20 \
+	persist.sys.use_dithering=0 \
+	persist.sys.purgeable_assets=0 \
+	windowsmgr.max_events_per_sec=240 \
+	view.touch_slop=2 \
+	view.minimum_fling_velocity=25 \
+	ro.additionalmounts=/storage/sdcard1 \
+	ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
+	persist.sys.vold.switchexternal=0 \
+	ro.device.cpu=1.0GHz_Cortex-A8_Allwinner_A10 \
+	ro.device.gpu=ARM_Mali_400 \
+	ro.device.rear_cam=None \
+	ro.device.front_cam=0.3_MP \
+	ro.device.screen_res=1024x768
+
+DEVICE_PACKAGE_OVERLAYS := device/allwinner/p85/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_PACKAGES += \
+	librs_jni
+
+# Hardware libs
+PRODUCT_PACKAGES += \
+	gralloc.sun4i \
+	sensors.sun4i \
+	hwcomposer.sun4i \
+	lights.sun4i \
+	display.sun4i \
+	power.sun4i \
+	audio.primary.sun4i \
+	audio.a2dp.default \
+	audio.usb.default \
+	libaudioutils \
+	chat \
+	u3gmonitor \
+	devlistener
+
+# Cedar libs
+PRODUCT_PACKAGES += \
+	libCedarA \
+	libaw_audioa \
+	libCedarX \
+	libcedarxsftstream \
+	libthirdpartstream \
+	libstagefright_soft_cedar_h264dec \
+	libcedarxosal \
+	libcedarxbase \
+	libcedarv \
+	libaw_audio \
+	libswdrm \
+	libcedarv_base \
+	libcedarv_adapter \
+	libve \
+	libfacedetection \
+	librtmp
+
+# CM9 apps
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory
+
+# EXT4 Support
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	e2fsck
+
+TARGET_BOOTANIMATION_NAME := horizontal-1024x768
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+# Should be after the full_base include, which loads languages_full
+PRODUCT_AAPT_CONFIG := normal large xlarge ldpi mdpi hdpi tvdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+PRODUCT_NAME := full_p85
+PRODUCT_DEVICE := p85
+
+#TARGET_SCREEN_HEIGHT := 768
+#TARGET_SCREEN_WIDTH := 1024
+
+
